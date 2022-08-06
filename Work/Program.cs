d93fc9d58b0bf382924c14202d8068668598d667
IgorShkatulo <shkatulo59@gmail.com>
2022-08-06 18:34:12 +0500

    Work rm

---
+++ Work/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] ArrayWords = new string[4] {"Hello", "2", "World", ":-)"};
+string[] ArrayWords2 = new string [ArrayWords.Length];
+
+void AskWords(string[] ArrayWords, string[] ArrayWords2)
+{
+    int count = 0;
+    for (int i = 0; i < ArrayWords.Length; i++)
+    {
+        if (ArrayWords[i].Length <= 3)
+        {
+            ArrayWords2[count] = ArrayWords[i];
+            count++;
+        }
+    }
+}
+
+void PrintArrayWords(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+    Console.WriteLine();
+
+}
+
+AskWords(ArrayWords, ArrayWords2);
+PrintArrayWords(ArrayWords2);
+//
