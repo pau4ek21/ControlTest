53f4879cbb8a8e0bc4668632ba229bbb652a4330
IgorShkatulo <shkatulo59@gmail.com>
2022-08-06 16:27:49 +0500

    Создали метод AskWords который находит силово у котрого длина <= 3 символа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] ArrayWords = new string[4] {"Hello", "2", "World", ":-)"};
 string[] ArrayWords2 = new string [ArrayWords.Length];
+
+void AskWords(string[] ArrayWords, string[] ArrayWords2)
+{
+    int count = 0;
+    for (int i = 0; i < ArrayWords.Length; i++)
+    {
+        if (ArrayWords[i] <= 3)
+        {
+            ArrayWords2[count] = ArrayWords[i];
+            count++;
+        }
+    }
+}
