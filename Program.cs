2a963d47c7795d93dc0c4f982db0edcd48e6fb95
IgorShkatulo <shkatulo59@gmail.com>
2022-08-06 16:33:16 +0500

    Создали метод PrintArrayWords который будет нам показывать результат

--- Program.cs
+++ Program.cs
@@ -13,3 +13,14 @@ void AskWords(string[] ArrayWords, string[] ArrayWords2)
         }
     }
 }
+
+void PrintArrayWords(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+    Console.WriteLine();
+
+}
